c838430f5eebf6a4efcb3d011caea8392c3a6ce6
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2025-03-22 09:54:45 +0000

    import sass style to home page

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Cards from "../../Components/Cards/Cards";
 import Nav from "../../Components/Nav/Nav";
 import Footer from "../../Components/Footer/Footer";
 import Clock from "../../Components/Clock/Clock";
+import "./Home.scss";
 
 function Home() {
   return (

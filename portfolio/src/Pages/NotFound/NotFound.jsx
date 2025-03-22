d747ee863fd9214afa42a3cd5e83ee1f9ab73787
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2025-03-22 10:17:10 +0000

    added sass style to NotFound modified code NotFound

--- portfolio/src/Pages/NotFound/NotFound.jsx
+++ portfolio/src/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,32 @@
-function NotFound() {
+import Nav from "../../Components/Nav/Nav";
+import Footer from "../../Components/Footer/Footer";
+import Clock from "../../Components/Clock/Clock";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./NotFound.scss";
+
+function NotFound({ initialSeconds = 3 }) {
+  const [secondsLeft, setSecondsLeft] = useState(initialSeconds);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (secondsLeft > 0) {
+      const timeout = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 3000);
+      return () => clearTimeout(timeout);
+    } else if (secondsLeft === 0) {
+      navigate("/");
+    }
+  }, [secondsLeft, navigate]);
   return (
     <div className="container">
       <Nav />
-      <h1>404</h1>
-      <p>Page not found</p>
+      <div className="not_found">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <p>redirecting to home page in {secondsLeft} seconds... </p>
+      </div>
       <Footer children={<Clock />} className={"footer"} />
     </div>
   );

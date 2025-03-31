ff69cb89d1e3ce8d0a053f1c9fe94093b1368cd6
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2025-03-31 14:46:56 +0000

    project component responsive added new projects to project component and fixed responsive

--- portfolio/src/Components/ProjectComponents/ProjectComponents.jsx
+++ portfolio/src/Components/ProjectComponents/ProjectComponents.jsx
@@ -4,9 +4,23 @@ import "./ProjectComponents.scss";
 import { useEffect } from "react";
 import { useRef } from "react";
 import { gsap } from "gsap";
+import { SiReactrouter } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+import { DiResponsive } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
 
 function ProjectComponents() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [title, setTitle] = useState("");
+
+  const handleTitle = (index) => {
+    setCurrentIndex(index);
+    setTitle(projectsDetails[index].title);
+  };
 
   const ref = useRef(null);
   useEffect(() => {
@@ -32,10 +46,18 @@ function ProjectComponents() {
       prevIndex === 0 ? projectsDetails.length - 1 : prevIndex - 1
     );
   };
-  const currentProject = projectsDetails[currentIndex];
+  const currentProject = projectsDetails[title ? currentIndex : 0];
 
-  return (
+  return title ? (
     <div ref={ref} className="project_box">
+      <button
+        id="close"
+        className="close"
+        type="button"
+        onClick={() => setTitle("")}
+      >
+        Back
+      </button>
       <div className="image_box">
         {projectsDetails.length > 1 && (
           <button
@@ -62,18 +84,42 @@ function ProjectComponents() {
 
       <div className="description_box">
         <h1 className="title">{currentProject.title}</h1>
+        <p>{currentProject.undertitle}</p>
         <ul className="description">
-          {currentProject.description.map((description, index) => (
-            <li key={index}>{description}</li>
+          {currentProject.technologies.map((technologie, title) => (
+            <li key={title}>{technologie}</li>
           ))}
         </ul>
+        {currentProject.description && <p>{currentProject.description}</p>}
         <ul className="link">
-          {currentProject.link.map((link, index) => (
+          {currentProject.link?.map((link, index) => (
             <li key={index}>
               <a href={link.url} target="_blank" rel="noopener noreferrer">
                 {link.url}
               </a>
               <span>{link.detail}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <div ref={ref} className="project_box">
+      <div className="link_box">
+        <ul className="link_project">
+          {projectsDetails.map((project, index) => (
+            <li key={project.title}>
+              <button
+                className="button_link"
+                type="button"
+                onClick={() => handleTitle(index)} // Pass the index
+              >
+                <img
+                  className="image_link"
+                  src={project.cover}
+                  alt={project.title}
+                />
+              </button>
             </li>
           ))}
         </ul>
--- portfolio/src/Data/Data.js
+++ portfolio/src/Data/Data.js
@@ -1,10 +1,24 @@
 import OhMyFood from "../assets/OhMyFood.webp";
 import Kasa from "../assets/Kasa.webp";
 import Portfolio from "../assets/Portfolio.webp";
+import ArgentBank from "../assets/ArgentBank.webp";
+import Booki from "../assets/Booki.webp";
+import { SiReactrouter } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+import { DiResponsive } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+
+
+
 
 export const projectsDetails = [
     {
-        "title": "Kasa, Real estate rental web application",
+        "title": "Kasa,",
+        "undertitle": "Real estate rental web application",
         "cover": Kasa,
         "link": [{
             "url": "https://ferdjani-brahim-8-qqhfmjtn1-brahims-projects-415e5c3b.vercel.app",
@@ -19,39 +33,70 @@ export const projectsDetails = [
             "detail": "Link to Github"
         },
         ],
-        "description": [
-            "REACT",
-            "SASS",
-            "Responsive"
-        ]
+        "technologies": [
+            "<FaReact size={30} />",
+            "<FaSass size={30} />",
+            "<DiResponsive size={30} />",
+            "<SiReactrouter size={30} />",
+            "<FaNodeJs size={30} />",
+        ],
+        "description": "Kasa is a dynamique and responsive web application using React and React Router. Thanks to SASS managing CSS and implementing CSS animations, the app is visually appealing."
     },
     {
-        "title": "OhMyFood, Improve a mobile site's interface with CSS animations",
+        "title": "OhMyFood,",
+        "undertitle": "Improve a mobile site's interface with CSS animations",
         "cover": OhMyFood,
         "link": [{ "url": "https://brahimferdjani.github.io/OhMyFood/", "detail": "Link to the site" },
         { "url": "https://www.figma.com/design/t4449fzDnwGYmzuwQdu87V/Maquettes-Ohmyfood-(mobile-et-desktop)", "detail": "Link to Figma" },
         { "url": "https://github.com/brahimferdjani/OhMyFood.git", "detail": "Link to Github" }],
-        "description": [
-            "HTML5",
-            "SASS",
-            "Responsive",
-            "Mobile-First",
-            "Menu Restaurant",
-            "Animations"
-        ]
+        "technologies": [
+            "<FaHtml5 size={30} />",
+            "<FaSass size={30} />",
+            "<DiResponsive size={30} />",
+        ],
+        "description": "OhMyFood is a mobile-first web interface, integrating CSS animations to enhance it's visual appeal but also to improve the user experience."
+
     },
     {
         "title": "Portfolio",
+        "undertitle": null,
         "cover": Portfolio,
         "link": [
             { "url": "https://github.com/brahimferdjani/Portfolio.git", "detail": "Link to Github" }],
-        "description": [
-            "REACT",
-            "SASS",
-            "Responsive",
-            "Portfolio"
-        ]
-    }
+        "technologies": [
+            "<FaReact size={30} />",
+            "<FaSass size={30} />",
+            "<DiResponsive size={30} />",
+        ],
+        "description": "My portfolio.",
+    },
+    {
+        "title": "Booki,",
+        "undertitle": "Improve a mobile site's interface with CSS animations",
+        "cover": Booki,
+        "link": [{ "url": "https://brahimferdjani.github.io/booki-starter-code-master/", "detail": "Link to the site" },
+        { "url": "https://github.com/brahimferdjani/booki-starter-code-master.git", "detail": "Link to Github" }],
+        "technologies": [
+            "<FaHtml5 size={30} />",
+            "<DiResponsive size={30} />",
+        ],
+        "description": "Booki is a responsive web interface. This project integrates designs accurately with HTML and CSS, setting up an efficient development environment."
+
+    },
+    {
+        "title": "ArgentBank,",
+        "undertitle": "Online bank application",
+        "cover": ArgentBank,
+        "link": [
+            { "url": "https://github.com/brahimferdjani/ArgentBank-Frontend-main.git", "detail": "Link to Github" }],
+        "technologies": [
+            "<FaReact size={30} />",
+            "<DiResponsive size={30} />",
+            "<SiRedux size={30} />",
+        ],
+        "description": "ArgentBank is a dynamic and responsive front end banking application. In this project I learned to create a web page by integrating the front end with the back end via API calls."
+
+    },
 ]
 
 console.log(projectsDetails);

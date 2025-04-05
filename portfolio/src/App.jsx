83647a40db9193c0af589c63777efa031d098617
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2025-04-05 19:27:37 +0000

    reverted last commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,9 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Contact from "./Pages/Contact/Contact";
 import NotFound from "./Pages/NotFound/NotFound";
-import Welcome from "./Pages/Welcome/Welcome";
 import AboutMe from "./Pages/AboutMe/AboutMe";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import Works from "./Pages/Works/Works";
 
 function App() {

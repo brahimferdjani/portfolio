970297ae17b32fb7457d8d53adfb6c95673b00d9
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2025-04-06 10:22:07 +0100

    deleted protected route b/c deployment issues

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,6 @@ import Contact from "./Pages/Contact/Contact";
 import NotFound from "./Pages/NotFound/NotFound";
 import AboutMe from "./Pages/AboutMe/AboutMe";
 import Works from "./Pages/Works/Works";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -13,38 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/menu"
-            element={
-              <ProtectedRoute>
-                <Menu />{" "}
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <ProtectedRoute>
-                <AboutMe />{" "}
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <ProtectedRoute>
-                <Contact />{" "}
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/works"
-            element={
-              <ProtectedRoute>
-                <Works />{" "}
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/works" element={<Works />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- portfolio/src/Components/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const token = sessionStorage.getItem("clicked");
-
-  return token ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
